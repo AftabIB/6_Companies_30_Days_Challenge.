e946776469df736365caf0a172eddf433c332bc2
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-01-02 13:06:41 +0530

    Time: 628 ms (5.26%) | Memory: 123.1 MB (42.11%) - LeetSync

---
+++ 2620-find-consecutive-integers-from-a-data-stream/find-consecutive-integers-from-a-data-stream.cpp
@@ -0,0 +1,32 @@
+class DataStream {
+public:
+    int count = 0;
+    int value,k;
+    DataStream(int value, int k) {
+        count = 0;
+        this -> value = value;
+        this -> k = k;
+    }
+    
+    bool consec(int num) {
+        if(num == value)
+        {
+            count++;
+        }  
+        else
+        {
+            count = 0;
+        }
+        if(count >= k)
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your DataStream object will be instantiated and called as such:
+ * DataStream* obj = new DataStream(value, k);
+ * bool param_1 = obj->consec(num);
+ */
