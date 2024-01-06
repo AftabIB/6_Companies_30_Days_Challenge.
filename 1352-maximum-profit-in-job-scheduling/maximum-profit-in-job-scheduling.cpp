52194522ea4ed596afe62d867f98024bc062e3e2
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-01-06 20:41:37 +0530

    Time: 243 ms (24.98%) | Memory: 99.5 MB (9.56%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -1,62 +1,54 @@
 class Solution {
 public:
     int n;
-    vector<int> dp;
+    unordered_map<int, int> memo;
 
-    int BS(vector<vector<int>> &array,int l,int target)
-    {
-        int r = n-1;
-
-        int result = n+1;
-        while(l <= r)
-        {
-            int mid = l + (r-l)/2;
-            if(array[mid][0] >= target)
-            {
+    int BS(vector<vector<int>>& array, int target) {
+        int l = 0, r = n - 1;
+        int result = n + 1;
+        
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (array[mid][0] >= target) {
                 result = mid;
                 r = mid - 1;
-            }
-            else
-            {
+            } else {
                 l = mid + 1;
             }
         }
         return result;
     }
 
-    int solve(vector<vector<int>>& array,int i)
-    {
-        if(i >= n)
-        {
+    int solve(vector<vector<int>>& array, int i) {
+        if (i >= n) {
             return 0;
         }
 
-        if(dp[i] != -1)
-        {
-            return dp[i];
+        if (memo.find(i) != memo.end()) {
+            return memo[i];
         }
 
-        int getNextJob = BS(array,i+1,array[i][1]);
-        int take = array[i][2] + solve(array,getNextJob);
-        int noTake = solve(array,i+1);
+        int getNextJob = BS(array, array[i][1]);
+        int take = array[i][2] + solve(array, getNextJob);
+        int noTake = solve(array, i + 1);
 
-        return dp[i] = max(take,noTake);
+        memo[i] = max(take, noTake);
+        return memo[i];
     }
 
     int jobScheduling(vector<int>& startTime, vector<int>& endTime,
                       vector<int>& profit) {
         n = startTime.size();
-        dp.resize(n, -1);
-        vector<vector<int>> array(n,vector<int>(3, 0));
-        
+        vector<vector<int>> array(n, vector<int>(3, 0));
+
         for (int i = 0; i < n; i++) {
             array[i][0] = startTime[i];
-            array[i][1] = endTime[i]; 
-            array[i][2] = profit[i]; 
+            array[i][1] = endTime[i];
+            array[i][2] = profit[i];
         }
-        
+
         sort(begin(array), end(array));
 
-        return solve(array,0);
+        return solve(array, 0);
     }
 };
