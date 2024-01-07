a6bdfaf5d60c748ad8084e8326501bcea253f4fd
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-01-07 11:29:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (37.83%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,46 +1,36 @@
 class Solution {
 public:
-int countStudents(vector<int> &A,int M)
-    {
-        int students = 1;
-        long long pageStudent = 0;
-        for(int i = 0;i<A.size();i++)
-        {
-            if(pageStudent + A[i] <= M)
-            {
-                pageStudent += A[i];
-            }
-            else
-            {
-                students += 1;
-                pageStudent = A[i];
+    int subArray(vector<int> &arr, int N, int num) {
+        int sum = 1;
+        long long element = 0;
+        for (int i = 0; i < N; i++) {
+            if (element + arr[i] <= num) {
+                element += arr[i];
+            } else {
+                sum += 1;
+                element = arr[i];
             }
         }
-        return students;
+        return sum;
     }
-    //Function to find minimum number of pages.
-    int findPages(vector<int> A,int N, int M) 
-    {
-        //code here
-        if(M > N)   return -1;
-        int low = *max_element(A.begin(),A.end());
-        int high = accumulate(A.begin(),A.end(),0);
-        while(low <= high)
-        {
-            int mid = low + (high - low)/2;
-            int students = countStudents(A,mid);
-            if(students > M)
-            {
+
+    int splitArray(vector<int> arr, int N, int K) {
+        if (K > N)
+            return -1;
+        int low = *max_element(arr.begin(), arr.end());
+        int high = accumulate(arr.begin(), arr.end(), 0);
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int element = subArray(arr, N, mid);
+            if (element > K) {
                 low = mid + 1;
-            }
-            else
-            {
+            } else {
                 high = mid - 1;
             }
         }
         return low;
     }
     int splitArray(vector<int>& nums, int k) {
-        return findPages(nums,nums.size(),k);
+        return splitArray(nums, nums.size(), k);
     }
 };
