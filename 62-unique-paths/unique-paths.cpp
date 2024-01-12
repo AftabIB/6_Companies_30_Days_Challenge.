fd8ba78b17eb54dc963dbb8eeaebb780f521b99f
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-01-12 09:20:55 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (68.25%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -23,7 +23,52 @@
 //     }
 // };
 
-// Tabulation
+// // Tabulation
+// class Solution {
+// public:
+//     int solve(int m, int n,vector<vector<int>> &dp)
+//     {
+//         if(m == 0 && n == 0)
+//             return 1;
+        
+//         if(m < 0 || n < 0)
+//             return 0;
+        
+//         if(dp[m][n] != -1)
+//             return dp[m][n];
+        
+//         int up = solve(m-1,n,dp);
+//         int left = solve(m,n-1,dp);
+
+//         return dp[m][n] = up + left;
+//     }
+//     int uniquePaths(int m, int n) {
+//         int dp[m][n];
+//         for(int i=0;i<m;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(i == 0 && j == 0)
+//                 {
+//                     dp[i][j] = 1;
+//                 }
+//                 else
+//                 {
+//                     int up = 0;
+//                     int left = 0;   
+//                     if(i > 0)
+//                         up = dp[i-1][j];
+//                     if(j > 0)
+//                         left = dp[i][j-1];
+//                     dp[i][j] = up + left;
+//                 }
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+// };
+
+// space optimization
 class Solution {
 public:
     int solve(int m, int n,vector<vector<int>> &dp)
@@ -43,28 +88,29 @@ public:
         return dp[m][n] = up + left;
     }
     int uniquePaths(int m, int n) {
-        int dp[m][n];
+        vector<int> prev(n,0);
         for(int i=0;i<m;i++)
         {
+            vector<int> curr(n,0);
             for(int j=0;j<n;j++)
             {
                 if(i == 0 && j == 0)
                 {
-                    dp[i][j] = 1;
+                    curr[j] = 1;
                 }
                 else
                 {
                     int up = 0;
                     int left = 0;   
                     if(i > 0)
-                        up = dp[i-1][j];
+                        up = prev[j];
                     if(j > 0)
-                        left = dp[i][j-1];
-                    dp[i][j] = up + left;
+                        left = curr[j-1];
+                    curr[j] = up + left;
                 }
             }
+            prev = curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
-
