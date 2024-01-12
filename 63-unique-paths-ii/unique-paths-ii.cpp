b7f89bd3cf8e0e75f091826172a39f991398fde6
Aftab Bhadgaonkar <122194015+AftabIB@users.noreply.github.com>
2024-01-12 10:01:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (24.72%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int solve(int i,int j,int& m,int& n,vector<vector<int>>& ob,vector<vector<int>> &dp){
-        if(i==m-1 && j==n-1)
+    int solve(int i,int j,vector<vector<int>>& ob,vector<vector<int>> &dp){
+        if(i==0 && j==0)
             return 1;
-        if(i>=m or j>=n)
+        if(i<0 or j<0)
             return 0;
         if(dp[i][j]!=-1)    return dp[i][j];
-        int right=0;
-        if(j+1<n && ob[i][j+1]!=1)
-            right=solve(i,j+1,m,n,ob,dp);
-        int down=0;
-        if(i+1<m && ob[i+1][j]!=1)    
-            down=solve(i+1,j,m,n,ob,dp);
+        int left=0;
+        if(j-1>=0 && ob[i][j-1]!=1)
+            left=solve(i,j-1,ob,dp);
+        int up=0;
+        if(i-1>=0 && ob[i-1][j]!=1)    
+            up=solve(i-1,j,ob,dp);
 
-        return dp[i][j]=right+down;
+        return dp[i][j]=left+up;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& ob) {
         int m = ob.size();
         int n = ob[0].size();
-        if(ob[0][0]==1) return 0;
+        if(ob[m-1][n-1]==1) return 0;
         vector<vector<int>> dp(m,vector<int>(n,-1));
 
-        return solve(0,0,m,n,ob,dp); 
+        return solve(m-1,n-1,ob,dp); 
     }
 };
